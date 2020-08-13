57e2622b69453589b6fc1017d17a0e705ccc71c9
badubizzle <badu.boahen@gmail.com>
2020-08-13 09:53:25 -0500

    Update boss_record_lib.erl

--- src/boss_record_lib.erl
+++ src/boss_record_lib.erl
@@ -1,5 +1,5 @@
 -module(boss_record_lib).
--export([run_before_hooks/2, 
+-export([run_before_hooks/2,
         run_after_hooks/3,
         run_before_delete_hooks/1,
         is_boss_record/2,
@@ -64,8 +64,8 @@ run_hooks(Record, Type, Function) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 is_boss_record(Record, ModelList) when is_tuple(Record) andalso is_atom(element(1, Record)) ->
     Type = element(1, Record),
-    lists:member(atom_to_list(Type), ModelList)            andalso 
-        erlang:function_exported(Type, attribute_names, 1) andalso 
+    lists:member(atom_to_list(Type), ModelList)            andalso
+        erlang:function_exported(Type, attribute_names, 1) andalso
         erlang:function_exported(Type, new, tuple_size(Record) - 1);
 is_boss_record(_, _) ->
     false.
@@ -79,29 +79,30 @@ dummy_record(Module) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 attribute_names(Module) ->
     DummyRecord = dummy_record(Module),
-    DummyRecord:attribute_names().
+    Module:attribute_names(DummyRecord).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 attribute_types(Module) ->
     DummyRecord = dummy_record(Module),
-    DummyRecord:attribute_types().
+    Module:attribute_types(DummyRecord).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 database_columns(Module) ->
     DummyRecord = dummy_record(Module),
-    DummyRecord:database_columns().
+    Module:database_columns(DummyRecord).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 database_table(Module) ->
     DummyRecord = dummy_record(Module),
-    DummyRecord:database_table().
+    Module:database_table(DummyRecord).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 belongs_to_types(Module) when is_atom(Module) ->
-    (dummy_record(Module)):belongs_to_types().
+    DummyRecord = dummy_record(Module),
+    Module:belongs_to_types(DummyRecord).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -125,9 +126,9 @@ convert_value_to_type(Val, integer) when is_binary(Val) ->
     list_to_integer(binary_to_list(Val));
 convert_value_to_type(Val, float) when is_binary(Val) ->
     binary_to_float(Val);
-convert_value_to_type(Val, float) when is_float(Val) -> 
+convert_value_to_type(Val, float) when is_float(Val) ->
     Val;
-convert_value_to_type(Val, float) when is_integer(Val) -> 
+convert_value_to_type(Val, float) when is_integer(Val) ->
     1.0 * Val;
 convert_value_to_type(Val, string) when is_integer(Val) ->
     integer_to_list(Val);
@@ -141,17 +142,17 @@ convert_value_to_type(Val, binary) when is_list(Val) ->
     list_to_binary(Val);
 convert_value_to_type(Val, binary) when is_binary(Val) ->
     Val;
-convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}}, Type) when is_integer(D1), is_integer(D2), is_integer(D3), 
+convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}}, Type) when is_integer(D1), is_integer(D2), is_integer(D3),
                                                                is_integer(T1), is_integer(T2), is_float(T3) ->
     convert_value_to_type({{D1, D2, D3}, {T1, T2, round(T3)}}, Type);
-convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, integer) when is_integer(D1), is_integer(D2), is_integer(D3), 
+convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, integer) when is_integer(D1), is_integer(D2), is_integer(D3),
                                                                         is_integer(T1), is_integer(T2), is_integer(T3) ->
-    calendar:datetime_to_gregorian_seconds(Val); 
-convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, timestamp) when is_integer(D1), is_integer(D2), is_integer(D3), 
+    calendar:datetime_to_gregorian_seconds(Val);
+convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, timestamp) when is_integer(D1), is_integer(D2), is_integer(D3),
                                                                           is_integer(T1), is_integer(T2), is_integer(T3) ->
     Secs = calendar:datetime_to_gregorian_seconds(Val) - calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
     {Secs rem ?MILLION, Secs div ?MILLION, 0};
-convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, datetime) when is_integer(D1), is_integer(D2), is_integer(D3), 
+convert_value_to_type({{D1, D2, D3}, {T1, T2, T3}} = Val, datetime) when is_integer(D1), is_integer(D2), is_integer(D3),
                                                                          is_integer(T1), is_integer(T2), is_integer(T3) ->
     Val;
 convert_value_to_type({D1, D2, D3} = Val, date) when is_integer(D1), is_integer(D2), is_integer(D3) ->
